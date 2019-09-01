9fb96a455e0db36da42bac87d1a109f076e5b86a
Chandra6160 <Chandra6160@gmail.com>
2019-09-01 20:01:45 +0530

    Modified show changes

--- src/components/home2.js
+++ src/components/home2.js
@@ -49,7 +49,7 @@ export default class HomeSecond extends React.Component {
                                     return (
                                         <tr >
                                             <td>
-                                                <Link to={`/show/${e.id}`}>{e.id}</Link>
+                                                <Link to={`/nav/show/${e.id}`}>{e.id}</Link>
                                             </td>
                                             <td scope="col">{e.email}</td>
                                             <td scope="col">{e.first_name}</td>
